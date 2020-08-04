544504684375825eeebcc5c33fe27fcb320b3818
madhunareddy <nareddymadhu777@gmail.com>
2020-08-04 14:22:52 -0400

    Madhu- Adding login page, Reset password page and more

---
+++ SecondCupCafe/app/src/main/java/com/example/secondcupcafe/Login.java
@@ -0,0 +1,27 @@
+package com.example.secondcupcafe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Login extends AppCompatActivity {
+    TextView resetpass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        resetpass=findViewById(R.id.textView3);
+        resetpass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),ResetPassword.class);
+                startActivity(i);
+            }
+        });
+    }
+}
