7bdafaa04665b5b693db3d0c054f1d0f6d2c0e06
Akhilkrishna415 <akhilkrishna185@gmail.com>
2020-08-04 16:06:06 -0400

    Akhil : Adding Splash screen

--- SecondCupCafe/app/src/main/java/com/example/secondcupcafe/AppLogo.java
+++ SecondCupCafe/app/src/main/java/com/example/secondcupcafe/AppLogo.java
@@ -1,14 +1,23 @@
 package com.example.secondcupcafe;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import  java.util.Timer;
+import java.util.TimerTask;
+import android.content.Intent;
 import android.os.Bundle;
-
 public class AppLogo extends AppCompatActivity {
-
+    Timer timer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_logo);
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(AppLogo.this, Login.class);
+                startActivity(intent);
+            }},3000);
     }
 }
+
