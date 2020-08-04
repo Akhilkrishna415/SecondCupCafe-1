77a132c1d5bdf8b6f0aa9db563a35cee08088523
Akhilkrishna415 <akhilkrishna185@gmail.com>
2020-08-04 16:38:05 -0400

    Akhil : Adding Intents to Signup page

--- SecondCupCafe/app/src/main/java/com/example/secondcupcafe/Signup.java
+++ SecondCupCafe/app/src/main/java/com/example/secondcupcafe/Signup.java
@@ -2,13 +2,34 @@ package com.example.secondcupcafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Signup extends AppCompatActivity {
+    Button b1;
+    Button b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        b1 = findViewById(R.id.button2);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Login.class);
+                startActivity(i);
+            }
+        });
+        b2 = findViewById(R.id.button);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Login.class);
+                startActivity(i);
+            }
+        });
     }
 }
