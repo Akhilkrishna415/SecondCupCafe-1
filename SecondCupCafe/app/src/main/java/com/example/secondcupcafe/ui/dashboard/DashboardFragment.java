4bed80f4dc4794e88027774b838a09753671d2b0
nikgrover <nikhil.grover20@gmail.com>
2020-08-04 12:35:34 -0400

    Adding order Ui and coffee menu Ui

--- SecondCupCafe/app/src/main/java/com/example/secondcupcafe/ui/dashboard/DashboardFragment.java
+++ SecondCupCafe/app/src/main/java/com/example/secondcupcafe/ui/dashboard/DashboardFragment.java
@@ -1,5 +1,6 @@
 package com.example.secondcupcafe.ui.dashboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,10 +9,12 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.secondcupcafe.CoffeeMenu;
 import com.example.secondcupcafe.R;
 
 public class DashboardFragment extends Fragment {
@@ -19,17 +22,47 @@ public class DashboardFragment extends Fragment {
     private DashboardViewModel dashboardViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+                            ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_dashboard,container,false);
+     CardView card,card2;
+        CardView card3,card4;
+
+        card = v.findViewById(R.id.coffeecard);
+        card.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), CoffeeMenu.class);
+                getActivity().startActivity(intent);
             }
         });
-        return root;
+
+      /*  card2 = v.findViewById(R.id.breakcard);
+        card2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), Breakfastv2.class);
+                getActivity().startActivity(intent);
+            }
+        });
+        card3 = v.findViewById(R.id.coldcard);
+        card3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), ColdBev.class);
+                getActivity().startActivity(intent);
+            }
+        });
+        card4 = v.findViewById(R.id.hotcard);
+        card4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), HotBev.class);
+                getActivity().startActivity(intent);
+            }
+        });
+
+*/
+        return v;}
+
     }
-}
+
