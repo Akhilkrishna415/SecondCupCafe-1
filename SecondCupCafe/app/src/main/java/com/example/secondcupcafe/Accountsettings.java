d25063d3f689a0f5b88fdb9b942f24b618a3739d
naveensadineni99 <naveensadineni7799@gmail.com>
2020-08-04 17:26:09 -0400

    modified in Accountsettings

--- SecondCupCafe/app/src/main/java/com/example/secondcupcafe/Accountsettings.java
+++ SecondCupCafe/app/src/main/java/com/example/secondcupcafe/Accountsettings.java
@@ -9,53 +9,10 @@ import android.widget.TextView;
 
 public class Accountsettings extends AppCompatActivity {
 
-    TextView personalSettings;
-    TextView rating;
-    TextView Contactus;
-    TextView Payment;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_settings);
-
-        personalSettings = findViewById(R.id.textView);
-        personalSettings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(), Personalsettings.class);
-                startActivity(i);
-            }
-        });
-        rating=findViewById(R.id.textView4);
-        rating.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(),Rating.class);
-                startActivity(i);
-
-            }
-        });
-        Contactus=findViewById(R.id.textView3);
-        Contactus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(),Contactus.class);
-                startActivity(i);
-
-            }
-        });
-        Payment=findViewById(R.id.textView2);
-        Payment.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(),Payment.class);
-                startActivity(i);
-            }
-        });
-
-
 
     }
 
--- SecondCupCafe/app/src/main/java/com/example/secondcupcafe/ui/notifications/NotificationsFragment.java
+++ SecondCupCafe/app/src/main/java/com/example/secondcupcafe/ui/notifications/NotificationsFragment.java
@@ -1,5 +1,6 @@
 package com.example.secondcupcafe.ui.notifications;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,7 +13,11 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.secondcupcafe.Contactus;
+import com.example.secondcupcafe.Payment;
+import com.example.secondcupcafe.Personalsettings;
 import com.example.secondcupcafe.R;
+import com.example.secondcupcafe.Rating;
 
 public class NotificationsFragment extends Fragment {
 
@@ -23,12 +28,51 @@ public class NotificationsFragment extends Fragment {
         notificationsViewModel =
                 ViewModelProviders.of(this).get(NotificationsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
+//        final TextView textView = root.findViewById(R.id.text_notifications);
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
 //                textView.setText(s);
             }
+        });
+
+        TextView personalSettings,payment,rating,contactUs;
+
+        personalSettings = root.findViewById(R.id.textView);
+        personalSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity().getApplicationContext(), Personalsettings.class);
+                startActivity(i);
+            }
+        });
+        rating=root.findViewById(R.id.textView4);
+        rating.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity().getApplicationContext(), Rating.class);
+                startActivity(i);
+
+            }
+        });
+        contactUs=root.findViewById(R.id.textView3);
+        contactUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity().getApplicationContext(), com.example.secondcupcafe.Contactus.class);
+                startActivity(i);
+
+            }
+        });
+        payment=root.findViewById(R.id.textView2);
+        payment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity().getApplicationContext(), Payment.class);
+                startActivity(i);
+            }
+
+
         });
         return root;
     }
