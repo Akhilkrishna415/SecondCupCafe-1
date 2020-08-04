58fbf8e5a3fe2c9972046c18e59d5432efdcb6f4
madhunareddy <nareddymadhu777@gmail.com>
2020-08-04 16:54:07 -0400

    Madhu- Adding intent to login page and toast message for reset password in reset password page

--- SecondCupCafe/app/src/main/java/com/example/secondcupcafe/ResetPassword.java
+++ SecondCupCafe/app/src/main/java/com/example/secondcupcafe/ResetPassword.java
@@ -2,13 +2,28 @@ package com.example.secondcupcafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class ResetPassword extends AppCompatActivity {
+    Button resetpwdbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
+        resetpwdbtn=findViewById(R.id.resetbtn);
+        resetpwdbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Login.class);
+                Toast.makeText(getApplicationContext(),"Confirmation link has been sent to your email!",Toast.LENGTH_SHORT).show();
+                startActivity(i);
+
+            }
+        });
     }
 }
