74f9483a6aaaaed2a385d6107e97cf40f9f5e6c0
madhunareddy <nareddymadhu777@gmail.com>
2020-08-04 16:36:58 -0400

    Madhu- Adding intent to Mainactivity page from login

--- SecondCupCafe/app/src/main/java/com/example/secondcupcafe/Login.java
+++ SecondCupCafe/app/src/main/java/com/example/secondcupcafe/Login.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
     TextView resetpass,signup;
+    Button loginbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,7 @@ public class Login extends AppCompatActivity {
 
         resetpass=findViewById(R.id.textView3);
         signup =findViewById(R.id.signup);
+        loginbtn = findViewById(R.id.login);
         resetpass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,6 +31,13 @@ public class Login extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(getApplicationContext(),Signup.class);
+                startActivity(i);
+            }
+        });
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),MainActivity.class);
                 startActivity(i);
             }
         });
