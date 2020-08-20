ae9aa57ae3ca0592d686bff74e8bbfbc1f67b42c
nikgrover <nikhil.grover20@gmail.com>
2020-08-20 09:03:06 -0400

    Added app icon and uploaded intent for call and mail button

--- SecondCupCafe/app/src/main/java/com/example/secondcupcafe/Contactus.java
+++ SecondCupCafe/app/src/main/java/com/example/secondcupcafe/Contactus.java
@@ -2,7 +2,10 @@ package com.example.secondcupcafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class Contactus extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class Contactus extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contactus);
+    }
+
+    public void call(View view)
+    {
+        Intent call = new Intent(Intent.ACTION_DIAL);
+        call.setData(Uri.parse("tel:5135699966"));
+        startActivity(call);
+    }
+
+    public void Mail(View view)
+    {
+        Intent Mail = new Intent(Intent.ACTION_SEND);
+        String[] recipients={"mailto@gmail.com"};
+        Mail.putExtra(Intent.EXTRA_EMAIL, recipients);
+        Mail.putExtra(Intent.EXTRA_SUBJECT,"Subject text here...");
+        Mail.putExtra(Intent.EXTRA_TEXT,"Body of the content here...");
+        Mail.putExtra(Intent.EXTRA_CC,"secondcupcafemontreal@gmail.com");
+        Mail.setType("text/html");
+        Mail.setPackage("com.google.android.gm");
+        startActivity(Intent.createChooser(Mail, "Send mail"));
     }
 }
