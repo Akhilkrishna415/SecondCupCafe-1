015e946252a66c7ce6110651b52b07ac645feea6
ryagasanjay <ryagasanjay@gmail.com>
2020-08-29 19:13:25 -0400

    Adding Breakfast menu files

--- SecondCupCafe/app/src/main/java/com/example/secondcupcafe/BreakfastAdapter.java
+++ SecondCupCafe/app/src/main/java/com/example/secondcupcafe/BreakfastAdapter.java
@@ -1,4 +1,87 @@
 package com.example.secondcupcafe;
 
-public class BreakfastAdapter {
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+//import com.squareup.picasso.Picasso;
+
+public class BreakfastAdapter extends RecyclerView.Adapter<BreakfastAdapter.ProductViewHolder> {
+
+    Context context;
+    List<BreakfastModel> breakfastlist;
+
+
+    public BreakfastAdapter(Context context, List<BreakfastModel> breakfastlist) {
+        this.context = context;
+        this.breakfastlist = breakfastlist;
+    }
+
+    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(context).inflate(R.layout.product_row_item, parent, false);
+        return new ProductViewHolder(view);
+    }
+
+    public void onBindViewHolder(@NonNull final ProductViewHolder holder, final int position) {
+
+//        Picasso.get().load(breakfastlist.get(position).getImageURL()).into(holder.prodImage);
+        holder.prodName.setText(breakfastlist.get(position).getTitle());
+        holder.prodPrice.setText(breakfastlist.get(position).getPrice() + " $");
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(context, BreakfastItemOne.class);
+
+                Bundle b = new Bundle();
+
+
+                String name = breakfastlist.get(position).getTitle();
+                String price = breakfastlist.get(position).getPrice();
+//                String image = breakfastlist.get(position).getImageURL();
+
+
+                b.putString("Name", name);
+                b.putString("Price", price);
+//                b.putString("Image", String.valueOf(image));
+
+
+                i.putExtras(b);
+                context.startActivity(i/*, activityOptions.toBundle()*/);
+            }
+        });
+
+
+    }
+
+
+    public int getItemCount() {
+        return breakfastlist.size();
+    }
+
+    public static final class ProductViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView prodImage;
+        TextView prodName, prodPrice;
+
+        public ProductViewHolder(@NonNull View itemView) {
+            super(itemView);
+//            prodImage = itemView.findViewById(R.id.product_img);
+            prodName = itemView.findViewById(R.id.item_name);
+            prodPrice = itemView.findViewById(R.id.item_price);
+
+        }
+    }
+
 }
--- SecondCupCafe/app/src/main/java/com/example/secondcupcafe/BreakfastItemOne.java
+++ SecondCupCafe/app/src/main/java/com/example/secondcupcafe/BreakfastItemOne.java
@@ -1,35 +1,104 @@
 package com.example.secondcupcafe;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
  * This class helps to add the breakfast item to the cart
+ *
  * @author naveen sadineni
  */
 
 public class BreakfastItemOne extends AppCompatActivity {
-Button order;
+    Button order;
+    FirebaseFirestore db;
+    private FirebaseUser curUser;
+    private FirebaseAuth auth;
+    TextView title, price;
+    EditText quantity, instructions;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_breakfast_item_one);
 
+        title = findViewById(R.id.Subtitle);
+        price = findViewById(R.id.item_pricev2);
+        quantity = findViewById(R.id.quantity);
+        instructions = findViewById(R.id.instructions);
+        db = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+        final Bundle b = getIntent().getExtras();
+
+        final String item_price = b.getString("Price");
+        final String item_name = b.getString("Name");
+//        final String p_image= b.getString("Image");
+        curUser = auth.getCurrentUser();
+
+
+        title.setText(item_name);
+        price.setText(item_price);
+
         order = findViewById(R.id.order);
         order.setOnClickListener(new View.OnClickListener() {
             @Override
             /**
-             * This method helps to move from breakfast screen to cart screen
+             * This method helps to move from breakfast screen to order screen
              */
 
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(),Cart.class);
-                startActivity(i);
+
+                final String userid = curUser.getUid();
+                final String phone = String.valueOf(curUser.getPhoneNumber());
+                final String email = String.valueOf(curUser.getEmail());
+                final String item_qty = quantity.getText().toString();
+                final String deliveryInstructions = instructions.getText().toString();
+
+                Map<String, Object> order = new HashMap<>();
+                order.put("userid", userid);
+                order.put("Email", email);
+//                order.put("phone", phone);
+                order.put("Item_name", item_name);
+                order.put("Item_price", item_price);
+                order.put("Quantitiy", item_qty);
+                order.put("Delivery_Instructions", deliveryInstructions);
+
+                db.collection("Orders").add(order).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(getApplicationContext(), "Your Order has been placed and is on your way.", Toast.LENGTH_LONG).show();
+
+                        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(i);
+                    }
+                })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w("ERROR", "Error writing document", e);
+                            }
+                        });
+
             }
         });
     }
